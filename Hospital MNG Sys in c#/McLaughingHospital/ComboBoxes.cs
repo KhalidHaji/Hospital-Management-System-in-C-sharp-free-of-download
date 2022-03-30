72c4848d530c1afa86ee0cc2061ac4df6e396fa9
Khalid Mohamed Abdi <80817031+KhalidHaji@users.noreply.github.com>
2022-03-30 21:35:05 +0600

    Add files via upload

---
+++ Hospital MNG Sys in c#/McLaughingHospital/ComboBoxes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AhsaniaHospital
+{
+    enum Counties
+    {
+        Dhaka,
+        Barishal,
+        Chittagong,
+        Mymensingh,
+        Khulna,
+        Rajshahi,
+        Rangpur,
+        Sylhet
+       
+
+    }
+    enum Specialties
+    {
+        Heart,
+        Lungs
+    }
+}
